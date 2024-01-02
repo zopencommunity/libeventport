b4811304b980d928b80856c3d749c6de5aebdeee
Igor Todorovski <itodorov@ca.ibm.com>
2024-01-02 16:03:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+libevent
+
+The libevent API provides a mechanism to execute a callback function when a specific event occurs on a file descriptor or after a timeout has been reached
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/libeventport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libeventport.git'), string(name: 'PORT_DESCRIPTION', value: 'The libevent API provides a mechanism to execute a callback function when a specific event occurs on a file descriptor or after a timeout has been reached' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
