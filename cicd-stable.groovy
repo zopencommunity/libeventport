b4520e1bb7eea6b735919a53702ddfb9f2311624
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:26 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/libeventport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/libeventport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libeventport.git'), string(name: 'PORT_DESCRIPTION', value: 'The libevent API provides a mechanism to execute a callback function when a specific event occurs on a file descriptor or after a timeout has been reached' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libeventport.git'), string(name: 'PORT_DESCRIPTION', value: 'The libevent API provides a mechanism to execute a callback function when a specific event occurs on a file descriptor or after a timeout has been reached' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
